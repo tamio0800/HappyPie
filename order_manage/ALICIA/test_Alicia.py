7e43dc7efb418400a7a18fe9d417d256d784fd40
tamio <tamio.chou@gmail.com>
2020-11-21 17:24:05 +0800

    測試ALICIA有收到我們丟出去的檔案

--- order_manage/ALICIA/test_Alicia.py
+++ order_manage/ALICIA/test_Alicia.py
@@ -1,17 +1,25 @@
 import unittest
 from Alicia_0611 import *
 
+
 class TestALICIA(unittest.TestCase):
     
     def setUp(self):
         self.alicia = ALICIA()
+        self.where_does_orders_locate = 'order_manage/ALICIA/temp_files/'
         print("Set up alicia")
 
     def tearDown(self):
         print("Done test.")
 
-    def test_failed(self):
-        self.assertEqual(1+1, 3)
+    def test_if_ALICIA_could_read_the_data(self):
+        if_files_are_all_good, exception_files = \
+            self.alicia.check_if_all_files_are_good_for_ALICIA_pipeline(
+                os.listdir(self.where_does_orders_locate)
+            )
+        self.assertEqual(if_files_are_all_good, True, 
+        f'Exception Files:\n{exception_files}')
+
 
 if __name__ == '__main__':
     unittest.main()
