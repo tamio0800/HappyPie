ce1da25762b209d4b1f74261c979712e44c4c2c1
tamio <tamio.chou@gmail.com>
2020-11-21 17:53:38 +0800

    將"穀粉"關鍵字列為堅果先生的產品

--- order_manage/ALICIA/test_Alicia.py
+++ order_manage/ALICIA/test_Alicia.py
@@ -7,19 +7,37 @@ class TestALICIA(unittest.TestCase):
     def setUp(self):
         self.alicia = ALICIA()
         self.where_does_orders_locate = 'order_manage/ALICIA/temp_files/'
+        self.test_order_file_name = '20201119_export_default (1).xls'
         print("Set up alicia")
 
     def tearDown(self):
         print("Done test.")
 
-    def test_if_ALICIA_could_read_the_data(self):
+    def test_if_ALICIA_could_see_the_data(self):
+        # 將訂單檔案放到給定的資料夾「self.where_does_orders_locate」，
+        # 確認ALICIA能不能看到這些資料
         if_files_are_all_good, exception_files = \
             self.alicia.check_if_all_files_are_good_for_ALICIA_pipeline(
                 os.listdir(self.where_does_orders_locate)
             )
-        self.assertEqual(if_files_are_all_good, True, 
-        f'Exception Files:\n{exception_files}')
+        self.assertIn(self.test_order_file_name, os.listdir(self.where_does_orders_locate))
+        # 先確認有看到要測試的資料
+        self.assertEqual(if_files_are_all_good, True, f'Exception Files:\n{exception_files}')
+        # 確認pipeline正確
+        
+    def test_if_ALICIA_could_read_the_data(self):
+        self.alicia.raw_txns_dir = self.where_does_orders_locate
+        # 給定要搜尋的路徑
+        platforms_found, platforms_not_found, after_alicia_exception_files = \
+            self.alicia._intergate_all_platforms()
+        self.assertGreater(len(platforms_found), 0)
+        self.assertIn('樂天派官網', platforms_found)
+        # 檢查是否有找到符合標準的訂單資料
 
+        
+        self.assertGreater(self.alicia.aggregated_txns.shape[0], 0)
+        # 檢查讀取到的內容
+        self.alicia.aggregated_txns.to_excel('order_manage/ALICIA/test_temp_excel.xlsx')
 
 if __name__ == '__main__':
     unittest.main()
